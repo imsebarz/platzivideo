a7bef4802e2729cdbea1343931c7c51cf0b83558
Sebastian Ruiz <sebastian.ruiz8@udea.edu.co>
2021-02-13 15:54:35 -0500

    Added categories section with carousels

---
+++ src/components/CarouselItem.jsx
@@ -0,0 +1,35 @@
+import React from "react";
+import "../assets/styles/components/CarouselItem.scss";
+import PlayIcon from "../assets/static/play-icon.png";
+import PlusIcon from "../assets/static/plus-icon.png";
+
+const CarouselItem = ({
+  title,
+  year,
+  cover,
+  duration,
+  description,
+  contentRating,
+}) => (
+  <div className="carousel-item">
+    <img className="carousel-item__img" src={cover} alt={title} />
+    <div className="carousel-item__details">
+      <div>
+        <img
+          className="carousel-item__details--img"
+          src={PlayIcon}
+          alt="Play Icon"
+        />
+        <img
+          className="carousel-item__details--img"
+          src={PlusIcon}
+          alt="Plus Icon"
+        />
+      </div>
+      <p className="carousel-item__details--title">{title}</p>
+      <p className="carousel-item__details--subtitle">{`${year} ${contentRating} ${duration}`}</p>
+    </div>
+  </div>
+);
+
+export default CarouselItem;
